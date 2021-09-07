a875b58db6e53697c70cb82af977c2987867e0a9
Daniel DeGuire <danjdeguire@gmail.com>
2021-09-07 17:14:30 -0500

    Update homework to include missing lines

--- week2homework.js
+++ week2homework.js
@@ -1,15 +1,19 @@
-// Inside your JS file, create two variables: pizzaPlace and numberOfToppings
-// Have the variables reference the name of your Pizza Place and the number of toppings your Pizza Place offers.
+// 4. Inside your JS file, create two variables: pizzaPlace and numberOfToppings
+// 5. Have the variables reference the name of your Pizza Place and the number of toppings your Pizza Place offers.
 
 let pizzaPlace = "Danno's Pizza Place";
 let numberOfToppings = 5;
 
-// Print the variables and their types.
+// 6. Print the variables and their types.
 
 console.log(pizzaPlace, "has", numberOfToppings, "toppings");
 console.log(typeof pizzaPlace, typeof numberOfToppings);
 
-// Print a template literal that uses both variables to construct a short sentence about your Pizza Place.
+// 7. Print a template literal that uses both variables to construct a short sentence about your Pizza Place.
+
+console.log(`Hello, welcome to ${pizzaPlace} we have ${numberOfToppings} toppings.`);
+
+// Construct an if statement that prints "Quality, not quantity." if you offer less than 10 toppings, or "A whole lot of pizza." if you offer 10 or more toppings.
 
 if (numberOfToppings < 10) {
         console.log("Quality, not quantity");
@@ -18,9 +22,9 @@ if (numberOfToppings < 10) {
     }
 // Use a loop to print all of the even numbers from 1 to numberOfToppings (or 10)
 
-for (let i = 1; i < numberOfToppings; i++) {
+for (let i = 1; i <= numberOfToppings; i++) {
     if (i % 2 == 0) {
-      console.log("Number of toppings:  " + i)
+      console.log(i)
 
     }
   }
