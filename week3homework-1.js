2a1f0b2baee3818c48b11bbddb7e6312b8c90360
Daniel DeGuire <danjdeguire@gmail.com>
2021-09-12 17:55:10 -0500

    Complete Week 3 Homework.

--- week3homework-1.js
+++ week3homework-1.js
@@ -1,56 +1,60 @@
 // Create an array of pizzaToppings with at least four different toppings
 
 let pizzaToppings = ["Pepperoni", "Sausage", "Chicken", "Bacon"];
+
 let pizzaSize = ["Small", "Medium", "Large"];
 let pizzaCrust = ["Thick", "Thin"];
+
 
 /* Create a greetCustomer function that prints a message that welcomes a guest, then informs them of the available toppings
 by looping over pizzaToppings (don't worry about perfect grammar here yet, i.e. "a, b, and c", see Bonus Challenge #9)
 i.e. "Welcome to Pizza House, our toppings are: a, b, c, ..." */
 
-function greetCustomer(a, b) {
-return a + b;
-}
-let x = greetCustomer("Welcome to Danno's Pizza House, we have ", pizzaToppings);
+function greetCustomer() {
+  return "Welcome to Danno's pizza. We have the following toppings " + pizzaToppings;
+};
+console.log(greetCustomer());
 
-console.log(x);
 
 
 /* Create a getPizzaOrder function that has the parameters size, crust, and an indefinite amount of toppings as inputs prints the order,
 i.e. "One large thick crust pizza with x, y, z, ... coming up!" outputs a list with the size, crust, and toppings. */
 
-function getPizzaOrder(a, b, c, d) {
-return a + b + c + d
+function getPizzaOrder() {
+  if (pizzaToppings.length > 0);
+  for (let pizzaToppings = 0; pizzaToppings < 5; pizzaToppings++);
+    console.log("Coming up: " + pizzaSize + pizzaCrust + pizzaToppings);
 }
-let y = getPizzaOrder("Coming up: One " + pizzaSize[0] + pizzaCrust[0] + pizzaToppings[0]);
+console.log(getPizzaOrder())
 
-
-console.log(y);
 
 
 /* Create a preparePizza function that has an array as its parameter with three items: a size, a crust, and a list of toppings
 prints something like "...Cooking pizza..." outputs a pizza Object with appropriate key-value pairs for size, crust, and toppings. */
 
-function getPizzaOrder(a, b, c, d) {
-  return a + b + c + d
-  }
-  let z = getPizzaOrder("Coming up: One " + pizzaSize[0] + pizzaCrust[0] + pizzaToppings[0]);
+function preparePizza() {
+  for (let pizzaToppings = 0; pizzaToppings < 5; pizzaToppings++);
+  return "Cooking pizza: " + pizzaSize + pizzaCrust + pizzaToppings
+};
 
-
-  console.log(z);
+  console.log(preparePizza());
 
 /* Create a servePizza function that has a parameter of a pizza Object logs a message that the pizza is ready and repeats the order,
 i.e. "Order up! Here's your large thick crust pizza with x, y, z, ... Enjoy!" outputs the same pizza Object that was passed in */
 
-function getPizzaOrder(a, b, c, d) {
-  return a + b + c + d
-  }
-  let z = getPizzaOrder("Coming up: One " + pizzaSize[0] + pizzaCrust[0] + pizzaToppings[0]);
+function servePizza() {
+  for (let pizzaToppings = 0; pizzaToppings < 5; pizzaToppings++);
+  return "Order ready: " + pizzaToppings + pizzaCrust + pizzaToppings
+};
 
-
-  console.log(z);
+  console.log(servePizza());
 
 // Call each function and (starting with preparePizza) use the returned value from the previous function as its input
+
+preparePizza(servePizza);
+
+// Figure out how to add "and" before the last topping in a list of toppings so when we list toppings it is more grammatically correct. You could create a listToppings function that stores the functionality, and call the function each time we list toppings in a function.
+// i.e. "Our toppings are a, b, and c." instead of "Our toppings are a, b, c"
 
 
 
