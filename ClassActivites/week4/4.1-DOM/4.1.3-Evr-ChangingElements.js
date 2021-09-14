62dc9b9f1173b072dc59dd4c00d3b9908d1f1900
Daniel DeGuire <danjdeguire@gmail.com>
2021-09-13 21:42:25 -0500

    Changing ELements

---
+++ ClassActivites/week4/4.1-DOM/4.1.3-Evr-ChangingElements.js
@@ -0,0 +1,28 @@
+// print the outer HTML of the navigation bar
+// const navBar = document.querySelector(".nav-bar");
+
+// console.log(navBar.outerHTML);
+
+// print the inner HTML of the navigation bar
+
+// console.log(navBar.innerHTML);
+
+// print the text content of the #class-schedule-list element
+
+// console.log(document.querySelector("#class-schedule-list element").textContent);
+
+// select the classList for the first class-week, then add the class "week-1"
+
+// const firstClassWeek = document.querySelector(".class-week");
+// console.log(firstClassWeek, "Before")
+// firstClassWeek.classList.add("week-1");
+// console.log(firstClassWeek, "After");
+
+
+// select the img element and add a src attribute
+
+document.querySelector("img").src =  "img.jpg";
+
+// change the font color of the h1 element
+
+document.querySelector("h1").style.color = "yellow";
